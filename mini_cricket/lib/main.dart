106dd74a3137b60a6cf134f5cefe8a03a052622f
Keshan Panditharathna <keshanebusiness@gmail.com>
2026-09-17 22:32:44 +0530

    image replace and code fixed

--- mini_cricket/lib/main.dart
+++ mini_cricket/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,26 +12,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mini Cricket',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the application theme. Then, without quitting the app, try changing
+        // the seedColor in the colorScheme below to Colors.green and then
+        // invoke "hot reload" (save your changes or press the "hot reload"
+        // button in a Flutter-supported IDE, or press "r" if you used
         // the command line to start the app).
         //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
+        // Notice that the game state doesn't reset during hot reload.
+        // To reset the state, use hot restart instead.
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Mini Cricket'),
     );
   }
 }
@@ -42,10 +43,10 @@ class MyHomePage extends StatefulWidget {
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  // This class is the configuration for the state. It holds the values
+  // provided by the parent (in this case the App widget) and used by the
+  // build method of the State. Fields in a Widget subclass are always
+  // marked "final".
 
   final String title;
 
@@ -54,68 +55,202 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // Total runs scored by the player.
+  int totalRun = 0;
 
-  void _incrementCounter() {
+  // Player starts with 6 balls.
+  int balls = 6;
+
+  // This variable displays the runs scored from the current ball.
+  String randomNo = '';
+
+  // List of possible runs that can be scored from one ball.
+  final List<int> runs = [0, 1, 2, 3, 4, 6];
+
+  // Random object is used to generate a random run.
+  final Random random = Random();
+
+  void bat() {
+    // Check whether there are balls remaining.
+    if (balls > 0) {
+      // Select a random run from the runs list.
+      int run = runs[random.nextInt(runs.length)];
+
+      setState(() {
+        // This call to setState tells the Flutter framework that something has
+        // changed in this State, which causes it to rerun the build method below
+        // so that the display can reflect the updated values.
+
+        // Reduce the number of balls by one.
+        balls = balls - 1;
+
+        // Add the randomly generated run to the total score.
+        totalRun = totalRun + run;
+
+        // Display the run scored from the current ball.
+        randomNo = '$run Runs';
+      });
+    }
+  }
+
+  void restart() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      // Reset all values to their initial values.
+      totalRun = 0;
+      balls = 6;
+      randomNo = '';
     });
   }
 
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
+    // by the bat and restart methods above.
     //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+        // TRY THIS: Try changing the color here to a specific color and
+        // trigger a hot reload to see the AppBar change color.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+
+      body: Container(
+        width: double.infinity,
+        color: Colors.green,
+
+        child: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the
+            // vertical axis because Columns are vertical.
+
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              // Row is used to display the bat and ball sections
+              // horizontally next to each other.
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+
+                children: [
+                  // ---------------- RUN SECTION ----------------
+                  Column(
+                    children: [
+                      Image.asset(
+                        'assets/images/bat.png',
+                        width: 180,
+                        height: 180,
+                        fit: BoxFit.contain,
+                      ),
+
+                      const SizedBox(height: 10),
+
+                      const Text(
+                        'Runs',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                      Text(
+                        '$totalRun',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  // SizedBox creates space between the two columns.
+                  const SizedBox(width: 50),
+
+                  // ---------------- BALL SECTION ----------------
+                  Column(
+                    children: [
+                      Image.asset(
+                        'assets/images/ball.png',
+                        width: 180,
+                        height: 180,
+                        fit: BoxFit.contain,
+                      ),
+
+                      const SizedBox(height: 10),
+
+                      const Text(
+                        'Balls',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                      Text(
+                        '$balls',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 30),
+
+              // Display the runs scored from the current ball.
+              Text(
+                randomNo,
+                style: const TextStyle(
+                  color: Color.from(alpha: 1, red: 1, green: 1, blue: 1),
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(height: 50),
+
+              // If balls are greater than 0, display the Bat button.
+              // Otherwise, display the Restart button.
+              balls > 0
+                  ? ElevatedButton(
+                      onPressed: bat,
+                      child: const Text('Bat'),
+                    )
+                  : ElevatedButton(
+                      onPressed: restart,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                        foregroundColor: Colors.white,
+                      ),
+                      child: const Text('Restart'),
+                    ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
